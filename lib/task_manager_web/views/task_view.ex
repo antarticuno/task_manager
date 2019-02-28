9c6b1e4b4528c66270ab30d82ca0d3b70500ba6b
Brendan <yang.b@husky.neu.edu>
2019-02-27 23:11:47 -0500

    aggregated display Tasks

--- lib/task_manager_web/views/task_view.ex
+++ lib/task_manager_web/views/task_view.ex
@@ -1,3 +1,17 @@
 defmodule TaskManagerWeb.TaskView do
   use TaskManagerWeb, :view
+
+  def assigned_users([]), do: "none"
+  def assigned_users(assigns_list) do
+    if (length(assigns_list) > 1) do
+      Enum.fetch!(assigns_list, 0).user.name <> " +#{(length(assigns_list) - 1)}"
+    else
+      Enum.fetch!(assigns_list, 0).user.name
+    end
+  end
+
+  def total_time(assigns_list) do
+    Enum.reduce(assigns_list, 0, &(&1.time_spent + &2))
+  end
+
 end
